f0e8d3686e64af04b488ef93b806da80f7e88533
trantiffany21 <tran.tiffany94@gmail.com>
2021-08-03 21:54:24 -0500

    Added functionality to buttons

--- js/app.js
+++ js/app.js
@@ -8,13 +8,35 @@ class Tomagotchi{
     displayStats(){
         const liStats = document.querySelectorAll('#statsList')
         liStats[0].innerHTML = `Hunger: ${this.hunger}`
-        liStats[1].innerHTML = `Sleep: ${this.sleep}`
-        liStats[2].innerHTML = `Boredom: ${this.boredom}`
+        liStats[1].innerHTML = `Boredom: ${this.boredom}`
+        liStats[2].innerHTML = `Sleep: ${this.sleep}`
         liStats[3].innerHTML = `Age: ${this.age}`
+        
       
+    }
+    feed(){
+        this.hunger--
+        this.displayStats()
+       
+    }
+    play(){
+        this.boredom--
+        this.displayStats()
+    }
+    lightsOff(){
+        this.sleep--
+        this.displayStats()
     }
 }
 
-const char = new Tomagotchi()
+const toma = new Tomagotchi()
 
-char.displayStats()
+toma.displayStats()
+const feedBtn = document.getElementById('feed')
+feedBtn.addEventListener("click", ()=>toma.feed())
+const playBtn = document.querySelector('#play')
+playBtn.addEventListener("click", ()=>toma.play())
+const sleepBtn = document.querySelector('#sleep')
+console.log(sleepBtn)
+sleepBtn.addEventListener("click", ()=>{
+    toma.lightsOff())
