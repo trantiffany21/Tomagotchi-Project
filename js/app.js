897ecdb2020d0c1dfd002a96298642c4ae74ebc9
trantiffany21 <tran.tiffany94@gmail.com>
2021-08-03 21:09:19 -0500

    update stats to method in class

--- js/app.js
+++ js/app.js
@@ -5,28 +5,16 @@ class Tomagotchi{
         this.boredom = 1
         this.age = 0
     }
-    getHunger(){
-        return this.hunger
-    }
-    getSleep(){
-        return this.sleep
-    }
-    getBoredom(){
-        return this.boredom
-    }
-    getAge(){
-        return this.age
+    displayStats(){
+        const liStats = document.querySelectorAll('#statsList')
+        liStats[0].innerHTML = `Hunger: ${this.hunger}`
+        liStats[1].innerHTML = `Sleep: ${this.sleep}`
+        liStats[2].innerHTML = `Boredom: ${this.boredom}`
+        liStats[3].innerHTML = `Age: ${this.age}`
+      
     }
 }
 
 const char = new Tomagotchi()
 
-const displayStats = () =>{
-    const liStats = document.querySelectorAll('#statsList')
-    liStats[0].innerHTML = `Hunger: ${char.getHunger()}`
-    liStats[1].innerHTML = `Sleep: ${char.getSleep()}`
-    liStats[2].innerHTML = `Boredom: ${char.getBoredom()}`
-    liStats[3].innerHTML = `Age: ${char.getAge()}`
-  
-}
-displayStats()
+char.displayStats()
