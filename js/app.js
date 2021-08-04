f3dfbf3b81bb45ac39313f24c9b058608ad4f30a
trantiffany21 <tran.tiffany94@gmail.com>
2021-08-03 20:37:33 -0500

    add character to screen, add buttons, and add stats text

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
 class Tomagotchi{
     constructor(){
-        this.hunger = 0
-        this.sleep = 0
-        this.boredom = 0
+        this.hunger = 1
+        this.sleep = 1
+        this.boredom = 1
         this.age = 0
+    }
+    getHunger(){
+        return this.hunger
+    }
+    getSleep(){
+        return this.sleep
+    }
+    getBoredom(){
+        return this.boredom
+    }
+    getAge(){
+        return this.age
     }
 }
 
 const char = new Tomagotchi()
+
+const displayStats = () =>{
+    const liStats = document.querySelectorAll('#statsList')
+    liStats[0].innerHTML = `Hunger: ${char.getHunger()}`
+    liStats[1].innerHTML = `Sleep: ${char.getSleep()}`
+    liStats[2].innerHTML = `Boredom: ${char.getBoredom()}`
+    liStats[3].innerHTML = `Age: ${char.getAge()}`
+  
+}
+displayStats()
