6f58a18ce3df5499c7e26d46944e9774d04bfcb8
trantiffany21 <tran.tiffany94@gmail.com>
2021-08-04 20:23:15 -0500

    Age up every 5 seconds, added comments

--- js/app.js
+++ js/app.js
@@ -28,26 +28,40 @@ class Tomagotchi{
         const screen = document.querySelector('#screen')
         screen.style.backgroundColor = "#346856"
         setTimeout(function(){
-            // some code
             screen.style.backgroundColor = "#88c070"
        },1000);
 
         this.displayStats()
     }
-    changeName(){
+    changeNameStartGame(){
         const name = document.querySelector("#name").value
         this.name = name
-        document.querySelector("h1").innerHTML = this.name     
+        document.querySelector("h1").innerHTML = this.name
+        
+        const aging = setInterval(()=>this.ageUp(),5000)
+    }
+    ageUp(){
+        this.age++
+        this.displayStats()
     }
 }
-
+//create tomagotchi
 const toma = new Tomagotchi()
 
+//display the stats
 toma.displayStats()
+
+//feed when clicked
 const feedBtn = document.getElementById('feed')
 feedBtn.addEventListener("click", ()=>toma.feed())
+
+//play when clicked
 const playBtn = document.querySelector('#play')
 playBtn.addEventListener("click", ()=>toma.play())
+
+//sleep and turn off lights when clicked
 const sleepBtn = document.querySelector('#sleep')
-console.log(sleepBtn)
 sleepBtn.addEventListener("click", ()=>toma.lightsOff())
+
+
+
