b770e6c45f322af906863b04ed7e82618071379d
trantiffany21 <tran.tiffany94@gmail.com>
2021-08-05 13:54:17 -0500

    Add game function to refactor code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,51 @@
+//game function
+const game = () => {
+        //create tomagotchi
+        const name = document.querySelector("#name").value
+        document.querySelector("h1").innerHTML = name
+
+        const toma = new Tomagotchi(name)
+
+        //feed when clicked
+        const feedBtn = document.getElementById('feed')
+        feedBtn.addEventListener("click", ()=>toma.feed())
+
+        //play when clicked
+        const playBtn = document.querySelector('#play')
+        playBtn.addEventListener("click", ()=>toma.play())
+
+        //sleep and turn off lights when clicked
+        const sleepBtn = document.querySelector('#sleep')
+        sleepBtn.addEventListener("click", ()=>toma.lightsOff())
+        
+        const aging = setInterval(()=>toma.ageUp(),5000)
+
+        //randomly change a stat within an interval of 0.5-2.5 seconds
+        const statChange = setInterval(()=>{
+                const randMethod = Math.floor(Math.random()*4)
+                console.log(randMethod)
+                if(randMethod === 0 ){
+                    toma.hungerUp()
+                }else if(randMethod === 1){
+                    toma.sleepUp()
+                }else{
+                    toma.boredomUp()
+                }
+                console.log("hunger", toma.hunger)
+                toma.displayStats()
+            },(Math.floor(Math.random()*2000)+500))
+
+}
+
 class Tomagotchi{
-    constructor(){
+    constructor(name){
+        this.name = name
         this.hunger = 1
         this.sleep = 1
         this.boredom = 1
         this.age = 0
         this.alive = true
+        this.displayStats()
     }
     displayStats(){
         const liStats = document.querySelectorAll('#statsList')
@@ -16,77 +57,46 @@ class Tomagotchi{
       
     }
     feed(){
-        this.hunger--
-        this.displayStats()
-       
+        if(this.hunger>1){
+            this.hunger--
+            this.displayStats()
+        }
     }
     play(){
-        this.boredom--
-        this.displayStats()
+        if(this.boredom>1){
+            this.boredom--
+            this.displayStats()
+        }
     }
     lightsOff(){
-        this.sleep--
-        const screen = document.querySelector('#screen')
-        screen.style.backgroundColor = "#346856"
-        setTimeout(function(){
-            screen.style.backgroundColor = "#88c070"
-       },1000);
-
-        this.displayStats()
+        if(this.sleep>1){
+            this.sleep--
+            const screen = document.querySelector('#screen')
+            screen.style.backgroundColor = "#346856"
+            setTimeout(function(){
+                screen.style.backgroundColor = "#88c070"
+                },1000);
+                this.displayStats()
+            }
     }
-    changeNameStartGame(){
-        const name = document.querySelector("#name").value
-        this.name = name
-        document.querySelector("h1").innerHTML = this.name
         
-        const aging = setInterval(()=>this.ageUp(),5000)
-        //randomly change a stat within an interval of 0-2 seconds
-        const statChange = setInterval(()=>{
-                const randMethod = Math.floor(Math.random()*4)
-                console.log(randMethod)
-                if(randMethod === 0 ){
-                    this.hungerUp()
-                }else if(randMethod === 1){
-                    this.sleepUp()
-                }else{
-                    this.boredomUp()
-                }
-            },(Math.floor(Math.random()*2000)))
-    }
+    
+    
     ageUp(){
         this.age++
         this.displayStats()
     }
     hungerUp(){
         this.hunger++
-        this.displayStats()
     }
     sleepUp(){
         this.sleep++
-        this.displayStats()
     }
     boredomUp(){
         this.boredom++
-        this.displayStats()
     }
 }
-//create tomagotchi
-const toma = new Tomagotchi()
 
-//display the stats
-toma.displayStats()
-
-//feed when clicked
-const feedBtn = document.getElementById('feed')
-feedBtn.addEventListener("click", ()=>toma.feed())
-
-//play when clicked
-const playBtn = document.querySelector('#play')
-playBtn.addEventListener("click", ()=>toma.play())
-
-//sleep and turn off lights when clicked
-const sleepBtn = document.querySelector('#sleep')
-sleepBtn.addEventListener("click", ()=>toma.lightsOff())
 
 
 
